e283e26ef1d79df02c47a9437b606277c32bbfc2
desktopbuddy <hanchondo21@gmail.com>
2023-08-31 00:07:59 -0700

    if statement for user input

--- index.js
+++ index.js
@@ -26,3 +26,17 @@ const questions = [
         name: "textcolor"
     }
 ];
+
+inquirer.prompt(questions).then(res=>{
+    let shape;
+    if (res.shape = "Circle"){
+        shape=new Circle(res.color, res.text, res.textcolor)
+    };
+    if (res.shape = "Square"){
+        shape=new Square(res.color, res.text, res.textcolor)
+    };
+    if (res.shape = "Triangle"){
+        shape=new Triangle(res.color, res.text, res.textcolor)
+    };
+    fs.writeFileSync("./examples/logo.svg",shape.render())
+});
