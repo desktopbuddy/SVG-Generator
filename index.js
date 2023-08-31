191a8462acfab3ff28ee8fdc652cdf960cd518ed
desktopbuddy <hanchondo21@gmail.com>
2023-08-30 23:19:53 -0700

    added inquirer questions

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const { listenerAdded } = require("emittery");
+
+const questions = [
+    {
+        type: "list",
+        message: "What shape do you want?",
+        choices: ["Circle", "Square", "Triangle"]
+        name: "shape"
+    },
+    {
+        type: "input",
+        message: "What color do you want your shape?",
+        name: "color"
+    },
+    {
+        type: "input",
+        message: "What letters would you like? (Input 3 characters)",
+        name: "text"
+    },
+    {
+        type: "input",
+        message: "What text color would you like?",
+        name: "textcolor"
+    }
+];
